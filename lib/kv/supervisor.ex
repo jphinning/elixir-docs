e091a20fdbbd155c41d24ca46639d896ac7f68a6
Joao Hinning <jphinning@gmail.com>
2025-07-25 20:30:56 -0300

    feat: Add supervisor

---
+++ lib/kv/supervisor.ex
@@ -0,0 +1,18 @@
+defmodule KV.Supervisor do
+  use Supervisor
+
+  ### Cliente
+  def start_link(opts) do
+    Supervisor.start_link(__MODULE__, :ok, opts)
+  end
+
+  ### Servidor
+  @impl true
+  def init(:ok) do
+    children = [
+      {KV.Registry, name: KV.Registry}
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end
