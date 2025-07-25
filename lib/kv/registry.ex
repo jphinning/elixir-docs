fc0119ba9ebe556cb1a684469a571f1a8229c3a2
Joao Hinning <jphinning@gmail.com>
2025-07-25 19:02:06 -0300

    feat: Added monitoring to the registry

--- lib/kv/registry.ex
+++ lib/kv/registry.ex
@@ -18,23 +18,51 @@ defmodule KV.Registry do
 
   @impl true
   def init(:ok) do
-    {:ok, %{}}
+    buckets_map = %{}
+    refs = %{}
+
+    {:ok, {buckets_map, refs}}
   end
 
   @impl true
-  def handle_call({:lookup, name}, _from, buckets_map) do
-    {:reply, Map.fetch(buckets_map, name), buckets_map}
+  def handle_call({:lookup, name}, _from, state) do
+    {buckets_map, _} = state
+    {:reply, Map.fetch(buckets_map, name), state}
   end
 
   @impl true
-  def handle_cast({:create, new_name}, buckets_map) do
-    updated_buckets_map =
-      buckets_map
-      |> Map.put_new_lazy(new_name, fn ->
-        {:ok, pid} = KV.Bucket.start_link([])
-        pid
-      end)
+  def handle_cast({:create, new_name}, state) do
+    {buckets_map, refs} = state
 
-    {:noreply, updated_buckets_map}
+    case Map.has_key?(buckets_map, new_name) do
+      true ->
+        {:noreply, state}
+
+      false ->
+        {:ok, bucket_pid} = KV.Bucket.start_link([])
+        ref = Process.monitor(bucket_pid)
+        refs = refs |> Map.put_new(ref, new_name)
+        buckets_map = buckets_map |> Map.put_new(new_name, bucket_pid)
+
+        {:noreply, {buckets_map, refs}}
+    end
+  end
+
+  @impl true
+  def handle_info({:DOWN, ref, :process, _pid, _reason}, state) do
+    {buckets_map, refs} = state
+
+    {name, refs} = refs |> Map.pop(ref)
+
+    buckets_map = buckets_map |> Map.delete(name)
+
+    {:noreply, {buckets_map, refs}}
+  end
+
+  @impl true
+  def handle_info(msg, state) do
+    require Logger
+    Logger.debug("Unexpected message in KV.Registry: #{inspect(msg)}")
+    {:noreply, state}
   end
 end
