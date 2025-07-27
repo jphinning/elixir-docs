3576472e6889018c4540bec2c77a35cc5793d308
Joao Hinning <jphinning@gmail.com>
2025-07-27 19:45:39 -0300

    feat: ETS being used in the registry

--- lib/kv/registry.ex
+++ lib/kv/registry.ex
@@ -3,60 +3,59 @@ defmodule KV.Registry do
   ## These are client functions
 
   def start_link(opts) do
-    GenServer.start_link(__MODULE__, :ok, opts)
+    server_process_name = Keyword.fetch!(opts, :name)
+    GenServer.start_link(__MODULE__, server_process_name, opts)
   end
 
-  def lookup(server, name) do
-    GenServer.call(server, {:lookup, name})
+  def lookup(server_process_name, bucket_name) do
+    case :ets.lookup(server_process_name, bucket_name) do
+      [{^bucket_name, pid}] -> {:ok, pid}
+      [] -> :error
+    end
   end
 
   def create(server, new_name) do
-    GenServer.cast(server, {:create, new_name})
+    GenServer.call(server, {:create, new_name})
   end
 
   ## These are server functions
 
   @impl true
-  def init(:ok) do
-    buckets_map = %{}
+  def init(server_process_name) do
+    bucket_table_name = :ets.new(server_process_name, [:named_table, read_concurrency: true])
     refs = %{}
 
-    {:ok, {buckets_map, refs}}
+    {:ok, {bucket_table_name, refs}}
   end
 
   @impl true
-  def handle_call({:lookup, name}, _from, state) do
-    {buckets_map, _} = state
-    {:reply, Map.fetch(buckets_map, name), state}
-  end
+  def handle_call({:create, new_name}, _from, state) do
+    {bucket_table_name, refs} = state
 
-  @impl true
-  def handle_cast({:create, new_name}, state) do
-    {buckets_map, refs} = state
+    case lookup(bucket_table_name, new_name) do
+      {:ok, pid} ->
+        {:reply, pid, {bucket_table_name, refs}}
 
-    case Map.has_key?(buckets_map, new_name) do
-      true ->
-        {:noreply, state}
-
-      false ->
+      :error ->
         {:ok, bucket_pid} = DynamicSupervisor.start_child(KV.BucketSupervisor, KV.Bucket)
         ref = Process.monitor(bucket_pid)
         refs = refs |> Map.put_new(ref, new_name)
-        buckets_map = buckets_map |> Map.put_new(new_name, bucket_pid)
 
-        {:noreply, {buckets_map, refs}}
+        :ets.insert(bucket_table_name, {new_name, bucket_pid})
+
+        {:reply, bucket_pid, {bucket_table_name, refs}}
     end
   end
 
   @impl true
   def handle_info({:DOWN, ref, :process, _pid, _reason}, state) do
-    {buckets_map, refs} = state
+    {bucket_table_name, refs} = state
 
     {name, refs} = refs |> Map.pop(ref)
 
-    buckets_map = buckets_map |> Map.delete(name)
+    :ets.delete(bucket_table_name, name)
 
-    {:noreply, {buckets_map, refs}}
+    {:noreply, {bucket_table_name, refs}}
   end
 
   @impl true
--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -1,38 +1,44 @@
 defmodule KV.RegistryTest do
   use ExUnit.Case, async: true
 
-  setup do
-    registry_pid = start_supervised!(KV.Registry)
-    %{registry: registry_pid}
+  setup context do
+    _registry_name = start_supervised!({KV.Registry, name: context.test})
+    %{registry: context.test}
   end
 
-  test "spawns buckets", %{registry: registry_pid} do
-    assert KV.Registry.lookup(registry_pid, "Config map") == :error
+  test "spawns buckets", %{registry: registry_name} do
+    assert KV.Registry.lookup(registry_name, "Config map") == :error
 
-    KV.Registry.create(registry_pid, "Shopping cart")
-    assert {:ok, bucket_pid} = KV.Registry.lookup(registry_pid, "Shopping cart")
+    KV.Registry.create(registry_name, "Shopping cart")
+    assert {:ok, bucket_pid} = KV.Registry.lookup(registry_name, "Shopping cart")
 
-    assert :ok = KV.Registry.create(registry_pid, "Shopping cart")
+    KV.Registry.create(registry_name, "Shopping cart")
 
     KV.Bucket.put(bucket_pid, "milk", 3)
     assert bucket_pid |> KV.Bucket.get("milk") == 3
   end
 
-  test "remove buckets on exit", %{registry: registry_pid} do
-    KV.Registry.create(registry_pid, "Shipping list")
+  test "remove buckets on exit", %{registry: registry_name} do
+    KV.Registry.create(registry_name, "Shipping list")
 
-    {:ok, bucket_pid} = KV.Registry.lookup(registry_pid, "Shipping list")
+    {:ok, bucket_pid} = KV.Registry.lookup(registry_name, "Shipping list")
 
     Agent.stop(bucket_pid)
-    assert KV.Registry.lookup(registry_pid, "Shipping list") == :error
+
+    ### Avoid race condition on processing the :DOWN message to exclude the bucket
+    _ = KV.Registry.create(registry_name, "bogus")
+    assert KV.Registry.lookup(registry_name, "Shipping list") == :error
   end
 
-  test "removes bucket on crash", %{registry: registry_pid} do
-    KV.Registry.create(registry_pid, "shopping")
-    {:ok, bucket_pid} = KV.Registry.lookup(registry_pid, "shopping")
+  test "removes bucket on crash", %{registry: registry_name} do
+    KV.Registry.create(registry_name, "shopping")
+    {:ok, bucket_pid} = KV.Registry.lookup(registry_name, "shopping")
 
     # Stop the bucket with non-normal reason
     Agent.stop(bucket_pid, :shutdown)
-    assert KV.Registry.lookup(registry_pid, "shopping") == :error
+
+    ### Avoid race condition on processing the :DOWN message to exclude the bucket
+    _ = KV.Registry.create(registry_name, "bogus")
+    assert KV.Registry.lookup(registry_name, "shopping") == :error
   end
 end
